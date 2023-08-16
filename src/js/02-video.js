4687ff2a68b49897aff5d913bc326555daa93b0b
MaksymSkala <maksymskala@gmail.com>
2023-08-17 00:39:37 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const vimeoPlayer = new Player('vimeo-player'); // Замість "vimeo-player" вкажіть свій id
+const vimeoPlayer = new Player('vimeo-player');
 const storageKey = 'videoplayer-current-time';
 
-// Функція для збереження часу відтворення у локальному сховищі
 const saveCurrentTimeToStorage = throttle(async (time) => {
   try {
     await localStorage.setItem(storageKey, time.toString());
   } catch (error) {
     console.error('Error saving current time to localStorage:', error);
   }
-}, 1000); // Оновлювати не частіше ніж раз на секунду
+}, 1000);
 
-// Отримання збереженого часу відтворення з локального сховища
 const getSavedTimeFromStorage = async () => {
   try {
     const savedTime = await localStorage.getItem(storageKey);
@@ -24,12 +22,10 @@ const getSavedTimeFromStorage = async () => {
   }
 };
 
-// Встановлення збереженого часу відтворення у плеєр
 getSavedTimeFromStorage().then((savedTime) => {
   vimeoPlayer.setCurrentTime(savedTime);
 });
 
-// Відстеження події оновлення часу відтворення
 vimeoPlayer.on('timeupdate', async (event) => {
   const currentTime = event.seconds;
   saveCurrentTimeToStorage(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+import throttle from 'lodash.throttle'; // Імпорт бібліотеки lodash.throttle
 
+const feedbackForm = document.querySelector('.feedback-form'); // Знаходимо форму
+const emailInput = feedbackForm.querySelector('input[name="email"]'); // Знаходимо поле Email
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]'); // Знаходимо поле Message
+
+const storageKey = 'feedback-form-state'; // Ключ для сховища
+
+// Функція для збереження даних у локальному сховищі
+const saveFormStateToStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  try {
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+  } catch (error) {
+    console.error('Error saving form state to localStorage:', error);
+  }
+}, 500); // Оновлювати не частіше ніж раз на 500 мілісекунд
+
+// Відстеження події input на полях форми
+emailInput.addEventListener('input', saveFormStateToStorage);
+messageTextarea.addEventListener('input', saveFormStateToStorage);
+
+// Функція для заповнення полів форми даними з локального сховища
+const populateFormFieldsFromStorage = () => {
+    try {
+      const formData = JSON.parse(localStorage.getItem(storageKey));
+      if (formData) {
+        emailInput.value = formData.email || '';
+        messageTextarea.value = formData.message || '';
+      }
+    } catch (error) {
+      console.error('Error populating form fields from localStorage:', error);
+    }
+  };
+  
+  // Заповнення полів форми при завантаженні сторінки
+  window.addEventListener('DOMContentLoaded', populateFormFieldsFromStorage);
+
+  feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+    const formData = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+  
+    // Очищення сховища та полів форми
+    localStorage.removeItem(storageKey);
+    emailInput.value = '';
+    messageTextarea.value = '';
+  
+    // Виведення даних у консоль
+    console.log('Form data submitted:', formData);
+  }); 
