afe2938f7e675a5bc4c76c4be205c2ce025525a9
MaksymSkala <maksymskala@gmail.com>
2023-08-17 00:50:09 +0300

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,11 @@
-import throttle from 'lodash.throttle'; // Імпорт бібліотеки lodash.throttle
+import throttle from 'lodash.throttle';
 
-const feedbackForm = document.querySelector('.feedback-form'); // Знаходимо форму
-const emailInput = feedbackForm.querySelector('input[name="email"]'); // Знаходимо поле Email
-const messageTextarea = feedbackForm.querySelector('textarea[name="message"]'); // Знаходимо поле Message
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
 
-const storageKey = 'feedback-form-state'; // Ключ для сховища
+const storageKey = 'feedback-form-state';
 
-// Функція для збереження даних у локальному сховищі
 const saveFormStateToStorage = throttle(() => {
   const formData = {
     email: emailInput.value,
@@ -18,13 +17,11 @@ const saveFormStateToStorage = throttle(() => {
   } catch (error) {
     console.error('Error saving form state to localStorage:', error);
   }
-}, 500); // Оновлювати не частіше ніж раз на 500 мілісекунд
+}, 500);
 
-// Відстеження події input на полях форми
 emailInput.addEventListener('input', saveFormStateToStorage);
 messageTextarea.addEventListener('input', saveFormStateToStorage);
 
-// Функція для заповнення полів форми даними з локального сховища
 const populateFormFieldsFromStorage = () => {
     try {
       const formData = JSON.parse(localStorage.getItem(storageKey));
@@ -35,9 +32,8 @@ const populateFormFieldsFromStorage = () => {
     } catch (error) {
       console.error('Error populating form fields from localStorage:', error);
     }
-  };
+};  
   
-  // Заповнення полів форми при завантаженні сторінки
   window.addEventListener('DOMContentLoaded', populateFormFieldsFromStorage);
 
   feedbackForm.addEventListener('submit', (event) => {
@@ -46,13 +42,11 @@ const populateFormFieldsFromStorage = () => {
     const formData = {
       email: emailInput.value,
       message: messageTextarea.value,
-    };
-  
-    // Очищення сховища та полів форми
+    };  
+    
     localStorage.removeItem(storageKey);
     emailInput.value = '';
-    messageTextarea.value = '';
-  
-    // Виведення даних у консоль
+    messageTextarea.value = '';  
+    
     console.log('Form data submitted:', formData);
-  }); 
+}); 
